69343c4fc0edc07d648ee017758a5e6d763243d9
alan rosales <alan.a.n.r.ar@gmail.com>
2023-11-01 21:35:42 -0300

    changes for first form

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -20,24 +20,28 @@ const page = () => {
     console.log(isVerified)
   }, [isVerified])
   return (
-    <div className="h-[1000px] w-full items-start justify-center flex bg-[#ffcfcf] py-10 text-white">
-      <form className="h-[auto] w-[90%] p-4 flex flex-col items-start rounded bg-[#361907]" onSubmit={() => alert(isVerified)}>
-        <div className="w-full p-4 flex row justify-start text-[2rem] items-center h-[100px] bg-[#490808]">
+    <div className="h-[100vh] min-h-[720px] w-full items-center justify-center flex bg-[#ffcfcf] py-4 text-white text-[1.2rem]">
+
+      <form className="h-[auto] w-[90%] p-4 py-10 flex flex-col items-center rounded bg-[#361907]" onSubmit={() => alert(isVerified)}>
+
+        <div className="w-full p-4 flex row justify-start text-[2rem] items-center h-[90px] bg-[#490808]">
             <h1>Registrate!</h1>
         </div>
 
-        <input type="text" placeholder="Nombre" className="p-4 mt-12 rounded bg-[#5f0a0a]" />
-        <input type="text" placeholder="Apellido" className="p-4 my-4 rounded bg-[#5f0a0a]" />
+        <input type="text" placeholder="Nombre" className="p-4 mt-10 rounded bg-[#5f0a0a] w-[90%]" />
+        <input type="text" placeholder="Apellido" className="p-4 my-4 rounded bg-[#5f0a0a] w-[90%]" />
 
-        <div className="bg-[#5f0a0a] w-[90%] h-[4px] my-8" />
+        <div className="bg-[#720f0f] w-[90%] h-[2px] my-4" />
 
-        <input type="password" placeholder="password" className="p-4 my-2 bg-[#5f0a0a]" />
-        <input type="confirm password" placeholder="password" className="p-4 my-2 bg-[#5f0a0a]" />
+        <input type="password" placeholder="password" className="p-4 my-2 bg-[#5f0a0a] w-[90%]" />
+        <input type="password" placeholder="confirm password" className="p-4 my-2 bg-[#5f0a0a] w-[90%]" />
 
-        <div className="w-full h-[auto] my-8 flex items-center justify-center">
-          <button className="bg-[#1b7423] py-4 w-[80%] rounded" type="submit">Siguiente</button>
+        <div className="w-full h-[auto] mt-8 flex items-center justify-center">
+          <button className="bg-[#1b7423] py-3 w-[90%] rounded" type="submit">Siguiente</button>
         </div>
+
       </form>
+
     </div>
 
   )
