f517fae47d523657269028093d3212f0bab1bf33
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-11-09 15:17:21 -0300

    .

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -30,7 +30,7 @@ const Page = () => {
       <div className="mb-20 w-full flex items-start justify-center lg:h-[100vh]">
         <div className="h-[auto] w-[95%] max-w-[350px] pb-8 pt-2 p-2 mt-[8vh] flex flex-col items-center rounded bg-[#2e180b]">
 
-          <div className="w-[95%] mt-2 p-3 flex row justify-start text-[1.7rem] items-center h-[10%] bg-[#611616] rounded">
+          <div className="w-[95%] mt-2 p-3 flex row justify-start text-[1.7rem] items-center h-[10%] bg-[#521c15] rounded">
             <h1>Registrate!</h1>
           </div>
           <div className="w-[90%] mt-1 h-auto flex items-center justify-end text-[0.9rem]">
