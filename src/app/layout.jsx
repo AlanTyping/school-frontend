920bb19240cb7b683b241a36b779aacdbfb2f723
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-11-10 11:38:30 -0300

    4k login responsive layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import ReactQueryProvider from '@/utils/QueryClientProvider';
+import ReactQueryProvider from '../utils/QueryClientProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
